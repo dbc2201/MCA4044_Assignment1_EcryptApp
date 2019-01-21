af12a7fc1748cac1cb8ac7bfa306b86dc81c4365
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-01-21 09:57:09 +0530

    init commit

---
+++ app/src/main/java/in/ac/gla/encrypt/MainActivity.java
@@ -0,0 +1,15 @@
+package in.ac.gla.encrypt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

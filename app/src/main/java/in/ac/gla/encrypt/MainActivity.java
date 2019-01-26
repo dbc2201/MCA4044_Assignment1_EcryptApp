35e75f1db238b778eecb7cecbf54749ceb2cd7d5
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-01-26 21:33:57 +0530

    remove UI

--- app/src/main/java/in/ac/gla/encrypt/MainActivity.java
+++ app/src/main/java/in/ac/gla/encrypt/MainActivity.java
@@ -2,14 +2,36 @@ package in.ac.gla.encrypt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText editText;
+    Button button;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String text =
+            }
+        });
+
     }
 }

0df7c6c103f7935607399ecad90ca9dcca0b2d7d
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-01-29 22:48:52 +0530

    add encryption for lowercase characters

--- app/src/main/java/in/ac/gla/encrypt/MainActivity.java
+++ app/src/main/java/in/ac/gla/encrypt/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private EditText etInput, etOutput;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,7 +18,39 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etInput = findViewById(R.id.et_input);
 
+        etOutput = findViewById(R.id.et_output);
 
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String output = "";
+                char key = 13;
+                String message = etInput.getText().toString().trim();
+                for (int i = 0; i < message.length(); i++)
+                {
+                    char input = message.charAt(i);
+                    if (input >= 'a' && input <= 'z')
+                    {
+                        input += key;
+                        if (input > 'z')
+                        {
+                            input -= 26;
+                        }
+                        if (input < 'a')
+                        {
+                            input += 26;
+                        }
+                        output = output.concat(Character.toString(input));
+                        etOutput.setText(output);
+                    }
+                }
+            }
+        });
     }
 }
